c6b2bd80f773fcdedba65ee5da6d3f78cbbab026
antporara <antporara@alum.us.es>
2024-11-11 23:45:16 +0100

    HOTFIX:  cambios en el esquema de la base de datos

--- gestion_reservas/models.py
+++ gestion_reservas/models.py
@@ -1,15 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Espacio(models.TextChoices):
+    # tupla para el enumerado
+    BALONCESTO = 'Baloncesto', 'Baloncesto'
+    FUTBOL = 'Fútbol', 'Fútbol'
+    PADEL = 'Padel', 'Padel'
+    PISCINA1 = 'Piscina1', 'Piscina1'
+    PISCINA2 = 'Piscina2', 'Piscina2'
 class Reserva(models.Model):
+    espacio=models.CharField(max_length=20, choices=Espacio.choices, default=Espacio.PADEL)
+    momento_inicio=models.DateTimeField()
+    momento_fin=models.DateTimeField()
+    user_id=models.ForeignKey(User, on_delete=models.CASCADE, related_name='reservas')
     created_at = models.DateTimeField(auto_now_add=True)
-    #client = models.ForeignKey('autenticacion.User', on_delete=models.CASCADE)
-    pista = models.ForeignKey('gestion_pistas.Pista', on_delete=models.CASCADE)
-    date = models.DateField()
     def __str__(self):
-        return f'{self.pista} - {self.date}'
-    @property
-    def total(self):
-        return self.pista.price if self.pista else 0.0
+        return f'Reserva {self.pk} para {self.espacio} de {self.momento_inicio} a {self.momento_fin}'
+   
     class Meta:
         db_table = 'reservas'
         verbose_name = 'reserva'
--- web_app/admin.py
+++ web_app/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-from gestion_reservas.models import Reserva, LineaReserva
-from gestion_pagos.models import Pago
-from gestion_pistas.models import Pista
+
 # Register your models here.
-admin.register(Reserva)
-admin.register(LineaReserva)
-admin.register(Pago)
-admin.register(Pista)
+
