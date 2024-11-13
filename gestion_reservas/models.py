f7e957ad36c0b3d9768630c39e78f6a423309e5b
antporara <antporara@alum.us.es>
2024-11-13 10:48:26 +0100

    MINORFIX: user en gestion pistas importado como get_user_model

--- gestion_reservas/models.py
+++ gestion_reservas/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+User=get_user_model()
 
 class Espacio(models.TextChoices):
     # tupla para el enumerado
